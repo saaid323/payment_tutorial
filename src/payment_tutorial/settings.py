cd4788a5d8a5b9b051914906f74cb0ebd08f8926
saaid <saaid2538@gmail.com>
2024-08-23 18:25:43 +0300

    correcting database configuration

--- src/payment_tutorial/settings.py
+++ src/payment_tutorial/settings.py
@@ -100,8 +100,6 @@ if DATABASE_URL is not None:
     DATABASES = {
         "default": dj_database_url.config(
             default=DATABASE_URL,
-            conn_max_age=300,
-            conn_health_checks=True,
             )
         }
 
